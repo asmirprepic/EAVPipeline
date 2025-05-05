ccd7e1d8d09029902b0fc9576ef36d0b56997e83
A P <asmirprepic@gmail.com>
2025-05-05 20:30:28 +0200

    Simulated some data

--- EAVPipeline.Core/Infrastructure/DataBaseInitializer.cs
+++ EAVPipeline.Core/Infrastructure/DataBaseInitializer.cs
@@ -18,12 +18,7 @@ public static class DataBaseInitializer {
         
         );
 
-        CREATE TABLE IF NOT EXISTS AttributeEnumOptions (
-            OptionId INTEGER PRIMARY KEY AUTOINCREMENT,
-            AttributeId INTEGER NOT NULL,
-            Value TEXT NOT NULL,
-            FOREIGN KEY (AttributeId) REFERENCES AttributeDefinitions(AttributeId)
-        );
+        
 
         CREATE TABLE IF NOT EXISTS AttributeValues (
         Id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -32,6 +27,17 @@ public static class DataBaseInitializer {
         Value TEXT, 
         ArchivedAt TEXT,
         FOREIGN KEY (AttributeId) References AttributeDefinitions (AttributeId)
+        );
+        
+         CREATE TABLE IF NOT EXISTS MasterEntities (
+            EntityId INTEGER PRIMARY KEY,
+            Name TEXT
+        );
+
+        CREATE TABLE IF NOT EXISTS RawInputData (
+            EntityId INTEGER NOT NULL,
+            PropertyKey TEXT NOT NULL,
+            PropertyValue TEXT NOT NULL
         );
         ";
         command.ExecuteNonQuery();
---
+++ EAVPipeline.Core/Infrastructure/EntitySimulator.cs
@@ -0,0 +1,28 @@
+using System.Data;
+using Dapper;
+
+public static class EntitySimulator{
+    public static void SeedEntitiesAndRawProperties(IDbConnection connection,int totalentities = 1_000_000,int propertiesPerEntity = 5){
+        var rnd = new Random();
+
+        Console.WriteLine("[INFO] Seedint entities and raw properties...");
+        using var transaction = connection.BeginTransaction();
+
+        for (int i = 1; i <= totalentities;i++){
+            connection.Execute("INSERT OR IGNORE INTO MasterEntities (EntityId,Name) VALUES (@EntityId,@Name)",
+            new {EntityId  = i, Name = $"Entity_{i}"},transaction);
+        
+
+            for (int j = 0;j < propertiesPerEntity; j++){
+                string key = $"property_{j+1}";
+                string value = $"val_{rnd.Next(1,100)}";
+
+                connection.Execute("INSERT INTO RawInputData (EntityId,PropertyKey,PropertyValue) VALUES (@EntityId,@PropertyKey,@PropertyValue)",
+                new {EntityId = i, PropertyKey = key, PropertyValue  = value},transaction
+                );
+            }
+    }
+    transaction.Commit();
+    Console.WriteLine("[INFO] Seeding complete.");
+    }
+}

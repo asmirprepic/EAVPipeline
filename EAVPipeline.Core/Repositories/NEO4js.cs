b96c52f07bd07bad01c946b9a6dda239ee9dfad8
A P <asmirprepic@gmail.com>
2025-05-22 23:57:28 +0200

    NEO4j stuff

---
+++ EAVPipeline.Core/Repositories/NEO4js.cs
@@ -0,0 +1,41 @@
+using Neo4j.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace EAVPipeline.Data.Repositories
+{
+    public class Neo4jRepository : IDisposable
+    {
+        private readonly IDriver _driver;
+
+        public Neo4jRepository(string uri, string user, string password)
+        {
+            _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+        }
+
+        public async Task CreateEntityNode(string entityName)
+        {
+            await using var session = _driver.AsyncSession();
+            var query = "CREATE (e:Entity {name: $name}) RETURN e";
+            var parameters = new { name = entityName };
+            await session.RunAsync(query, parameters);
+        }
+
+        public async Task<List<string>> GetAllEntities()
+        {
+            await using var session = _driver.AsyncSession();
+            var query = "MATCH (e:Entity) RETURN e.name";
+            var result = await session.RunAsync(query);
+            var entities = new List<string>();
+            await result.ForEachAsync(record => entities.Add(record[0].As<string>()));
+            return entities;
+        }
+
+        public void Dispose()
+        {
+            _driver?.Dispose();
+        }
+    }
+}
